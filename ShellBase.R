856d24d1b56d673058fde2780b365e14efa6bddf
huangwb8 <654751191@qq.com>
2023-11-01 18:05:58 +0800

    Optimize DownloadFASTQFromEBI.R

--- ShellBase.R
+++ ShellBase.R
@@ -188,3 +188,34 @@ mergeMatrixDup <- function(x,
 
 
 
+
+#' @title Is one True?
+#' @description Is one True?
+#' @param vt a vector
+#' @return logic
+#' @author Weibin Huang<\email{654751191@@qq.com}>
+#' @examples
+#' vt <- c(T,"2","1","3");is.one.true(vt)
+#' vt <- c(F,"2","1","3");is.one.true(vt)
+#' vt <- c(T,2,1,3);is.one.true(vt)
+#' vt <- c(T,F);is.one.true(vt)
+#' vt <- c(F,F);is.one.true(vt)
+#' @export
+is.one.true <- function(vt){
+ if(!is.logical(vt)){
+   #vt不全是逻辑值
+   if(!is.numeric(vt)){
+     #vt不是数值型变量
+     l1 <- T %in% vt
+     return(l1)
+   } else {
+     #vt是数值型向量
+     print("向量是数值型，无法区分。")
+   }
+ } else {
+   #vt是逻辑型向量
+   l1 <- T %in% vt
+   return(l1)
+ }
+
+}
--- example/DownloadFASTQFromEBI.R
+++ example/DownloadFASTQFromEBI.R
@@ -1,7 +1,7 @@
 
 
 #============Information
-# Version: 0.1.2
+# Version: 0.1.10
 # Author: Weibin Huang
 
 #============Usage
@@ -46,45 +46,62 @@ library(optparse);
 
 # Function: downloader
 # wget, aspera
-fastq_downloader <- function(path_fastq, url, method){
+fastq_downloader <- function(path_fastq, url, method, losse_check){
   
   # url = "fasp.sra.ebi.ac.uk:/vol1/fastq/SRR138/028/SRR13816328/SRR13816328.fastq.gz"
-  LuckyVerbose(accession, ': Downloading ', url,'...')
   url_base = basename(url)
-  
-  exit_code <- -1
-  
+
   # Download fastq.gz
   repeat {
     
+    # Shell expression
     if(method == 'aspera'){
       # ~/.aspera/connect/bin/ascp -l 1000M -P 33001 -QT -k 2 -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh era-fasp@fasp.sra.ebi.ac.uk:/vol1/fastq/SRR138/028/SRR13816328/SRR13816328.fastq.gz ./test.fq.gz
       url_e = paste0('~/.aspera/connect/bin/ascp -l 1000M -P 33001 -QT -k 2 -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh era-fasp@', url, ' ', path_fastq,'/',url_base)
+      if(losse_check & file.exists(paste0(path_fastq,'/',url_base))){
+        url_e = 'completed'
+      }
     } else if(method == 'wget'){
       url_e = paste0('wget -c -o ',path_fastq,'/wget_', url_base, '.log -T 120 -t inf --directory-prefix=',path_fastq,' ', url)
     } else {
-      url_e <- NULL
+      url_e <- 'wrongmethod'
     }
+    
     
     # Download
-    system(url_e, wait = TRUE)
-    
-    # Get exit code
-    exit_code <- system("echo $?", intern = TRUE)
-    exit_code <- as.integer(exit_code)
-    
-    # Success or failure behavior
-    if (exit_code == 0) {
-      # Game over
-      LuckyVerbose(accession, ": ",url,' completed!')
+    if(url_e == 'completed'){
+      
+      LuckyVerbose(accession, ': ', url_base, ' had been download completely. Ignored!')
       break
+      
+    } else if(url_e == 'wrongmethod'){
+      
+      LuckyVerbose(accession, ": Please use right method. One of wget and aspera.")
+      break
+      
     } else {
-      # Another repeat after 120s
-      Sys.sleep(120)
-      LuckyVerbose(accession, ': Repeat Downloading ', url_base ,'...')
+      
+      # Download the url
+      LuckyVerbose(accession, ': Downloading ', url,'...')
+      result <- tryCatch(system(url_e, intern = TRUE), error = function(e) e) # wait = TRUE is default
+      error_msg <- as.character(unlist(result))
+      is_repeat <- is.one.true(grepl("Error|Partial|Fail", error_msg, ignore.case = T))
+      if(is_repeat){
+        LuckyVerbose(accession, ": ascp encountered an error. Retrying...")
+        Sys.sleep(120) # Another repeat after 120s
+      } else {
+        LuckyVerbose(accession, ": ",url_base,"'s ascp result: ")
+        print(result)
+        LuckyVerbose(accession, ": ",url_base,' completed!')
+        break
+      }
+      
     }
+    
   }
+  
 }
+
 
 #============Parameters
 
@@ -95,7 +112,8 @@ if(T){
   option_list <- list(
     make_option(c("-a", "--accession"), type = "character", default = NULL, help = "The accession ID of a GEO project. "),
     make_option(c("-p", "--path_project"), type = "character", default = NULL, help = "The path of project"),
-    make_option(c("-m", "--method"), type = "character", default = 'aspera', help = "One of 'aspera', 'wget'")
+    make_option(c("-m", "--method"), type = "character", default = 'aspera', help = "One of 'aspera', 'wget'"),
+    make_option(c("-l", "--losse_check"), action = "store_true", default = TRUE, help = "Wheter to ignore files with suffix 'fastq.gz'. Only works in aspera method!")
   )
   
   # Shell to R
@@ -106,7 +124,8 @@ if(T){
   path_project = opt$path_project
   path_fastq = paste0(path_project,'/', 'output/fastq')
   method = opt$method
-
+  losse_check = opt$losse_check
+  
 } else {
   
   # R parameters
@@ -114,6 +133,7 @@ if(T){
   path_project = 'E:/PythonCloud/@Ubuntu/test/GSE167977'
   path_fastq = paste0(path_project,'/output/fastq')
   method = 'aspera'
+  losse_check = T
 }
 
 #============Programe
@@ -143,7 +163,7 @@ fq_urls <- as.character(df$fastq_aspera)
 for(urlP in fq_urls){
   # urlP = "fasp.sra.ebi.ac.uk:/vol1/fastq/SRR588/002/SRR5885322/SRR5885322_1.fastq.gz;fasp.sra.ebi.ac.uk:/vol1/fastq/SRR588/002/SRR5885322/SRR5885322_2.fastq.gz"
   urls <- Fastextra(urlP, ';')
-  for(url in urls) fastq_downloader(path_fastq, url, method)
+  for(url in urls) fastq_downloader(path_fastq, url, method, losse_check)
 }
 
 # Game over
